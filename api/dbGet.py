f00d542acc2cdc4bea55f0506dff1043a8ab9cea
Shane Loewe <shaneloewe@gmail.com>
2023-04-02 23:21:23 -0600

    hid mongo connection string in .env var

--- api/dbGet.py
+++ api/dbGet.py
@@ -1,10 +1,12 @@
 from pymongo import MongoClient
+import os
+from decouple import config
 
 
 def get_database():
 
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb+srv://shaneloewe:CoffeeCup4@cluster0.quxitcu.mongodb.net/test"
+    CONNECTION_STRING = config('MONGODB_SECRET')
 
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     client = MongoClient(CONNECTION_STRING)
